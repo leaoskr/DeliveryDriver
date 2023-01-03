3ae0f1d62c5a3470e67fa203f0cce870e856aa0f
YuQian Shi <yuqian.shi@yahoo.com>
2023-01-03 09:24:21 -0500

    test collision and trigger and add assets for the game

--- deliverydriverGame/Assets/Driver.cs
+++ deliverydriverGame/Assets/Driver.cs
@@ -9,13 +9,15 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
